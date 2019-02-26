3cce321894a3dea0f2497040ed943669c3621c0b
joaopscastro <jpedroc96@gmail.com>
2019-02-26 10:47:30 +0000

    Implement line multiplication in Go

--- GoLang/main.go
+++ GoLang/main.go
@@ -10,7 +10,6 @@ import (
 func MatrixMult(line, col int) {
 
 	var temp int
-	// var pha, phb, phc []int
 
 	pha := make([]int, line*col)
 	phb := make([]int, line*col)
@@ -53,6 +52,55 @@ func MatrixMult(line, col int) {
 	}
 }
 
+//MatrixMultLine makes the line multiplication
+func MatrixMultLine(line, col int) {
+
+	// var temp int
+
+	pha := make([]int, line*col)
+	phb := make([]int, line*col)
+	phc := make([]int, line*col)
+
+	for i := 0; i < line; i++ {
+		for j := 0; j < col; j++ {
+			pha[i*line+j] = 1.0
+		}
+	}
+	for i := 0; i < line; i++ {
+		for j := 0; j < col; j++ {
+			phb[i*line+j] = i + 1.0
+		}
+	}
+	for i := 0; i < line; i++ {
+		for j := 0; j < col; j++ {
+			phc[i*line+j] = 0.0
+		}
+	}
+
+	start := time.Now()
+
+	for i := 0; i < line; i++ {
+		for k := 0; k < col; k++ {
+			for j := 0; j < line; j++ {
+				phc[i*line+j] += pha[i*line+k] * phb[k*col+j]
+			}
+		}
+	}
+
+	elapsed := time.Since(start)
+	log.Printf("Time: %s seconds\n", elapsed)
+
+	// fmt.Printf("%v", phc)
+	for i := 0; i < 10; i++ {
+		fmt.Println(phc[i])
+	}
+}
+
+//MatrixMultBlock makes the block multiplication
+func MatrixMultBlock(line, col int) {
+	//TODO
+}
+
 func main() {
 	var line, col, op int
 
@@ -71,10 +119,18 @@ func main() {
 			MatrixMult(line, col)
 		}
 		if op == 2 {
+			fmt.Print("Dimensions? Line&Col: \n")
+			fmt.Scanln(&line)
+			col = line
 
+			MatrixMultLine(line, col)
 		}
 		if op == 3 {
+			fmt.Print("Dimensions? Line&Col: \n")
+			fmt.Scanln(&line)
+			col = line
 
+			MatrixMultBlock(line, col)
 		}
 	}
 }
